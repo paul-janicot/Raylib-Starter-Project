7a6b64bc4b3cd8721041d2248473bf3410dbf8d9
Paul Janicot <paul.janicot@artfx.fr>
2025-03-27 17:26:27 +0100

    [add] music

--- Pong.cpp
+++ Pong.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 //-----Const---------------
-    const int x = 500;
+    const int x = 800;
     const int y = 500;
     
     const int margeY = 50;
@@ -15,6 +15,12 @@ int main() {
     const int sizeOfPadel = 100;
     const int widthOfPadel = 10;
 
+    const int padelSpeed = 10;
+
+    
+
+    const Vector2 scorePosition1{ x *0.30, y / 10 };
+    const Vector2 scorePosition2{ x*0.70, y / 10 };
     
 //--------Variables------------------
     int padelPositionX1 = 50;
@@ -33,6 +39,9 @@ int main() {
     int movementX = 8;
     int movementY = 8;
 
+    int score1 = 0;
+    int score2 = 0;
+
 //----------------------------
 
 
@@ -43,46 +52,97 @@ int main() {
 
     //Init
 
+    InitAudioDevice();
+    Music lvlMusic = LoadMusicStream("Sounds/Bloopin.mp3");
+    PlayMusicStream(lvlMusic);
+    bool pause = false;
+
+    Font ft = LoadFont("resources/fonts/alagard.png");
+    Image img = LoadImage("resources/images/background.png");
+    Texture2D texture = LoadTextureFromImage(img);      // Image converted to texture, uploaded to GPU memory (VRAM)
+    UnloadImage(img);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
 
 
     while (!WindowShouldClose()) {
         //Update
 
         if (IsKeyDown('W') &&(padelPositionY1 >= margeY)){
-            padelPositionY1 -= 10;
+            padelPositionY1 -= padelSpeed;
         }
         else if (IsKeyDown('S') && padelPositionY1 <= y-margeY-sizeOfPadel){
-            padelPositionY1 += 10;
+            padelPositionY1 += padelSpeed;
+        }
+        if (IsKeyDown(KEY_UP) && (padelPositionY2 >= margeY)) 
+        {
+            padelPositionY2 -= padelSpeed;
+        }
+        if (IsKeyDown(KEY_DOWN) && (padelPositionY2 <= y - margeY - sizeOfPadel))
+        {
+            padelPositionY2 += padelSpeed;
+        }
+
+        UpdateMusicStream(lvlMusic);
+        if (IsKeyPressed(KEY_SPACE)) {
+            pause = !pause;
+            if (pause) StopMusicStream(lvlMusic);
+            else ResumeMusicStream(lvlMusic);
         }
 
 
+
+        // Mise à jour de la position de la balle
         ballX += movementX;
         ballY += movementY;
 
-        if (ballX <= radius || ballX >= x - radius) {
-            movementX *= -1;
-      
-
-           
-        }
+        // Rebond sur les murs haut et bas
         if (ballY <= radius || ballY >= y - radius) {
             movementY *= -1;
         }
-        else if ((ballX <= padelPositionX1+widthOfPadel + radius) && ((ballY >= padelPositionY1) && (ballY >= padelPositionY1-sizeOfPadel))){
+
+        // Collision avec la raquette gauche (padel 1)
+        if ((ballX - radius <= padelPositionX1 + widthOfPadel) &&
+            (ballY >= padelPositionY1 && ballY <= padelPositionY1 + sizeOfPadel)) {
             movementX *= -1;
         }
+
+        // Collision avec la raquette droite (padel 2)
+        if ((ballX + radius >= padelPositionX2) &&  // Vérifie si la balle touche le bord gauche de la raquette 2
+            (ballY >= padelPositionY2 && ballY <= padelPositionY2 + sizeOfPadel)) {
+            movementX *= -1;
+        }
+
+        if (ballX <= 0) {
+            score2 += 1;
+            ballX = x * 0.75;
+        }
+        if (ballX >= x) {
+            score1 += 1;
+            ballX = x / 4;
+        }
+
+       
         
 
         BeginDrawing();
+        
+
         ClearBackground(BLACK);
 
         //Draw
+        DrawTexture(texture, 0, 0, WHITE);
         DrawRectangle(padelPositionX1, padelPositionY1, widthOfPadel, sizeOfPadel, WHITE);
         DrawRectangle(padelPositionX2, padelPositionY2, widthOfPadel, sizeOfPadel, WHITE);
         DrawCircle(ballX, ballY, radius, WHITE);
+        DrawTextEx(ft, TextFormat("%i", score1), scorePosition1, 40, 5, WHITE);
+        DrawTextEx(ft, TextFormat("%i", score2), scorePosition2, 40, 5, WHITE);
+        DrawLine(x / 2, 0, x / 2, y, WHITE);
+
+       
+
         EndDrawing();
     }
 
+    UnloadMusicStream(lvlMusic);
     CloseWindow();
     return 0;
 }
